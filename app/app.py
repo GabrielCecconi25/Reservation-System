4879e285979cb0786c7966573a3c4a06c822aa82
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-05-23 15:04:27 -0300

    feat: adicionando rotas para salas e reservas no app

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
 from flask import jsonify, request
 from config import app, db
+from controller.sala import salasApp
+from controller.reserva import reservasApp  # novo import do controller de reservas
+
+# Registro dos Blueprints
+app.register_blueprint(salasApp, url_prefix='/')
+app.register_blueprint(reservasApp, url_prefix='/salas')  # adiciona o blueprint de reservas
 
 # Criação das tabelas no banco
 with app.app_context():
